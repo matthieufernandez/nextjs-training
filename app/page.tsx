92bcc33a4cbe12e66679e8136f34a99762231df7
matt <mrmatthieufernandez@gmail.com>
2023-07-28 13:05:55 -0400

    added dynamic search

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main className={inter.className}>
-      <div className="text-white"><h1>Home Page</h1></div>
+      <div className="text-white p-4 "><h1>Home Page</h1></div>
     </main>
   )
 }
